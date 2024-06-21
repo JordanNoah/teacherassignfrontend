d6c2bc4564e439b41cc56f61eda842d2ccff301a
jordan.ubilla <jordan.ubilla@funiber.org>
2024-06-21 16:47:27 -0500

    init proyect

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,20 @@
+import {
+    getCurrentInstance
+} from "vue"
+
+import * as directives from "vuetify/directives"
+import * as components from "vuetify/components"
+import {
+    createVuetify
+} from "vuetify"
+
+// Styles
+import "vuetify/styles"
+import "@mdi/font/css/materialdesignicons.css"
+
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
+export default vuetify
